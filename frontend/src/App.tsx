0669481981467e9a9dce98d01b8d1425ad06ec0d
EZ4BluePoison <you@example.com>
2025-03-17 20:28:21 +1100

    Make a basic navigation of 3 icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react'
-import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import UltraVioletPage from './pages/UltraVioletPage';
-import SplashScreen from './pages/SplashScreen';
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom"; // ⬅ 只导入 Routes 和 Route
+
+import Navbar from "./components/Navbar";
+import BottomNavigation from "./components/BottomNavigation";
+import HomePage from "./pages/HomePage";
+import UltraVioletPage from "./pages/UltraVioletPage";
+import SplashScreen from "./pages/SplashScreen";
 
 function App() {
-
   const [showSplashScreen, setShowSplashScreen] = useState(true);
 
   useEffect(() => {
-
     setTimeout(() => {
       setShowSplashScreen(false);
     }, 2000);
-  })
+  }, []);
+
   return (
     <>
-      {showSplashScreen ?
-        (<SplashScreen/> ) :
-        (
-          <div data-theme="synthwave">
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage/>} />
-                <Route path="/uvpage/" element={<UltraVioletPage/>} />
-            </Routes>
-          </div>
-        )
-      }
-      
+      {showSplashScreen ? (
+        <SplashScreen />
+      ) : (
+        <div data-theme="synthwave">
+          <Navbar />
+          <Routes> {/* ✅ 这里的 Routes 仍然保留 */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/uvpage" element={<UltraVioletPage />} />
+          </Routes>
+          <BottomNavigation />
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
